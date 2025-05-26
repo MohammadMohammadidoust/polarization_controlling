c1eb59e223aae91096429df600f8a6eece6d7b6a
ICQTs_laptop <info@icqts.ir>
2025-05-26 15:35:36 +0330

    Adding load saved models example

---
+++ examples/load_model_controling_polarisation_dqn_optimiser_rigol.py
@@ -0,0 +1,89 @@
+import time
+import json
+import logging
+from logs import LoggingConfiguration
+from instruments.Oscilloscopes import RIGOL
+from instruments.PolarizationControllers import OzOptics
+from optimizers.Optimizers import DQN
+CONFIG_FILE = "../CONFIG.json"
+
+with open (CONFIG_FILE, 'r') as j_file:
+    CONFIGS = json.load(j_file)
+
+LoggingConfiguration.configure_logging(CONFIGS["logging"])
+logger = logging.getLogger("main")
+
+
+def scope_configuration(brand= "RIGOL", auto_set= False, source_channel= 1):
+    scope = RIGOL(CONFIGS)
+    logger.debug(f"scope with address {scope.resource_address} start configuring")
+    print("Scope Address: ", scope.resource_address)
+    if auto_set:
+        scope.auto_set_device()
+        print("auto set is done!")
+    print("wait for initialisation!")
+    scope.initialise()
+    time.sleep(5)
+    return scope
+
+
+def p_controller_configuration(brand= "OzOptics"):
+    p_controller = OzOptics(CONFIGS)
+    p_controller.connect()
+    return p_controller
+
+
+
+controller = p_controller_configuration()
+acquirer = scope_configuration()
+optimizer = DQN(CONFIGS, acquirer, controller)
+
+
+#loading_model
+MODEL_NAME = "backup.h5"
+TARGET_MODEL_NAME = "dddqn_target_model.h5"
+optimizer.load_model(MODEL_NAME)
+
+
+acquirer.capture()
+acquirer.visualise(acquirer.scaled_data)
+
+acquirer.smoother()
+acquirer.visualise(acquirer.smoothed_data)
+
+i, f = acquirer.extract_period_index_v4(s_channel= 1)
+acquirer.clean_wave_form_data(i, f)
+#acquirer.visualise(acquirer.cleaned_data)
+type_one = acquirer.discriminator(s_channel= 1)
+print("type one wave: ", type_one)
+acquirer.qber_calculator(type_one)
+print("hv_qber: ",acquirer.hv_qber)
+print("pm_qber: ",acquirer.pm_qber)
+print("qber: ",acquirer.qber)
+
+
+
+
+counter = 0
+logger.info("start running main programme")
+while True:
+    try:
+        acquirer.update_data(additional_data= [None, None, None, None], source_channel= 1)
+        #print("live pm_qber: ",acquirer.pm_qber)
+        #print("live hv_qber: ",acquirer.hv_qber)
+        #if acquirer.qber < 0.1:
+        #    acquirer.visualise(acquirer.cleaned_data)
+        #    print("type one wave: ", acquirer.discriminator(s_channel= 1))
+        #    acquirer.visualise(acquirer.smoothed_data)
+        print("Live QBER: ", acquirer.qber)
+        if 0.095 < acquirer.qber:
+            print("start running optimiser")
+            time.sleep(0.4)
+            optimizer.run()
+        time.sleep(0.4)
+        counter += 1
+        if counter % 100 == 0:
+            #acquirer.visualise(acquirer.cleaned_data)
+            acquirer.extract_results("new_learning_20km_.csv")
+    except KeyboardInterrupt:
+        logger.info("the programme has been finished")
